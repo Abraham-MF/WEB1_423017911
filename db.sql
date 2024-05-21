1e6ee73872d3bf7138b70e9a8da3992ab4f82f72
Abraham-MF <159219728+Abraham-MF@users.noreply.github.com>
2024-05-21 14:33:04 -0600

    Delete db.sql

--- db.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `persona`(
-`nombre_usuario` text not null,
-`apellido_usuario` text not null,
-`mail` varchar(100) not null,
-`telefono` varchar(10) not null,
-
-)engine=Innodb default charset=utf8;
