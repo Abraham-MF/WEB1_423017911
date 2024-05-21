fbb105eebd7ae82964b376deb85c69b38147a64e
Abraham-MF <159219728+Abraham-MF@users.noreply.github.com>
2024-05-21 14:31:37 -0600

    Add files via upload

---
+++ db.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `persona`(
+`nombre_usuario` text not null,
+`apellido_usuario` text not null,
+`mail` varchar(100) not null,
+`telefono` varchar(10) not null,
+
+)engine=Innodb default charset=utf8;
