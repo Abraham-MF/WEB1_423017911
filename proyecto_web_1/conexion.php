b560702bf19f4e62ff5c5640129524d5075d2e95
Abraham-MF <159219728+Abraham-MF@users.noreply.github.com>
2024-05-21 16:25:42 -0600

    Update conexion.php

--- proyecto_web_1/conexion.php
+++ proyecto_web_1/conexion.php
@@ -3,3 +3,5 @@ $host_db = "sql101.infinityfree.com";
 $user_db = "if0_36150769";
 $password_db = "2BCObHhkZ60N1GH";
 $db_name = "if0_36150769_proyecto_web";
+
+$conexion = new mysqli($host_db, $user_db, $password_db, $db_name);
