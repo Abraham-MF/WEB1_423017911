3872fa3e48b75352a93e271b5202428c92e0e688
Abraham-MF <159219728+Abraham-MF@users.noreply.github.com>
2024-05-21 15:41:58 -0600

    Update enviarRegistro.php

--- proyecto_web_1/enviarRegistro.php
+++ proyecto_web_1/enviarRegistro.php
@@ -4,3 +4,5 @@ mysqli_set_charset($conexion, "utf8");
 
 $nombre_usuario = $_POST['nombre_usuario'];
 $apellido_usuario = $_POST['apellido_usuario'];
+mail = $_POST['mail'];
+$telefono = $_POST['telefono'];
