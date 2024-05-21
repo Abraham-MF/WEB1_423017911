3983ec8a430640776d67162458e1687dbb226955
Abraham-MF <159219728+Abraham-MF@users.noreply.github.com>
2024-05-21 14:44:50 -0600

    Update index.php

--- proyecto_web_1/index.php
+++ proyecto_web_1/index.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="estilos.css">
     <title>Document</title>
 </head>
-<body>
+<body class="fondo">
 nav class="nav">
         <p>Yana Games</p>
            <div class="ddiv">
@@ -23,6 +23,19 @@ nav class="nav">
         
     </div>
 </nav>
+
+    <section id="Home" style="display: flex; justify-content: center; align-items: center;">
+    <table>
+        <tr>
+            <td>
+                <h1 style="font-size: 50px; text-align: center;" >Yana Games</h1>
+
+                <p style="font-size: 30px;">trae para ti la llega anticipada de juegos muy esperados
+                por toda la comunidad</p>
+            </td>
+        </tr>
+    </table>
+</section>
 
   
 </body>
