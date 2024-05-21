a47d06653d6db86b31bf5dd2233df5384e3eac7a
Abraham-MF <159219728+Abraham-MF@users.noreply.github.com>
2024-05-21 14:43:58 -0600

    Update index.php

--- proyecto_web_1/index.php
+++ proyecto_web_1/index.php
@@ -3,10 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos.css">
     <title>Document</title>
 </head>
 <body>
+nav class="nav">
+        <p>Yana Games</p>
+           <div class="ddiv">
 
+            <a href="#Home">Home</a>
+            <a href="#seccion 1">seccion 1</a>
+            <a href="#seccion 2">seccion 2</a>
+            <a href="#seccion 3">seccion 3</a>
+            <a href="#seccion 4">seccion 4</a>
+            <a href="#seccion 5">seccion 5</a>
+
+
+        <a href="Formulario.html">registrame</a>
+        
+    </div>
+</nav>
 
   
 </body>
